f93edae4560603b15cec2f99917f48a1a10a895f
Colin Harker <53018170+ColinHarker@users.noreply.github.com>
2022-02-03 13:56:48 -0500

    Added Image, and reformated activity name

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
